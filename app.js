12687cde3ed65faf38966ab0ec4871511bbc210b
Tovmas <tovmas.tadevosyan.007@gmail.com>
2025-02-14 20:51:38 +0400

    sghjhgf

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const hostname = '127.0.0.1';
 
 const server = http.createServer((req, res) => {
     try{
+        res.statusCode = 200;
         console.log('Server started');
     }catch(err){
+        res.statusCode = 404;
         console.error(err);
+        console.log('Server failed.');
     }
 })
