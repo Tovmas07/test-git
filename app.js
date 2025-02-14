ce28040533fd733056584fa9b2603e47a40b4d47
Tovmas <tovmas.tadevosyan.007@gmail.com>
2025-02-14 19:49:01 +0400

    minor change

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ import http from 'http';
 const port = 3001;
 const hostname = '127.0.0.1';
 
+const server = http.createServer((req, res) => {
+    try{
+        console.log('Server started');
+    }catch(err){
+        console.error(err);
+    }
+})
